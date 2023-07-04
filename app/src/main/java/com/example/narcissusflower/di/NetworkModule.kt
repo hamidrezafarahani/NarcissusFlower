5c933fb6eb6b63ff2d9bee188eaaee996e986b02
HamidReza Farahani <h.farahani>
2023-07-04 11:40:04 +0430

    add network module

--- app/src/main/java/com/example/narcissusflower/di/NetworkModule.kt
+++ app/src/main/java/com/example/narcissusflower/di/NetworkModule.kt
@@ -1,10 +1,110 @@
 package com.example.narcissusflower.di
 
+import android.app.Application
+import com.example.narcissusflower.data.remote.UnSplashService
+import com.facebook.shimmer.BuildConfig
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+
+private const val BASE_URL = "https://api.unsplash.com/"
+
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Provides
+    fun provideGson(): Gson {
+        return GsonBuilder().create()
+    }
+
+    @Provides
+    @LoggingInterceptor
+    fun provideLoggingInterceptor(): Interceptor {
+        return HttpLoggingInterceptor {
+            Timber.tag("log-okhttp").d(it)
+        }.apply {
+            level = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        }
+    }
+
+    @Provides
+    @AuthInterceptor
+    fun provideAuthInterceptor(): Interceptor {
+        return Interceptor {
+            val url = it.request().url.newBuilder().apply {
+                // TODO: add query parameter value
+                addQueryParameter("client_id", "")
+            }.build()
+            val request = it.request().newBuilder()
+                .url(url)
+                /*.addHeader("Authorization", "Bearer ....")*/
+                .build()
+            it.proceed(request)
+        }
+    }
+
+    @Provides
+    fun provideOkHttpCache(app: Application): Cache {
+        return Cache(app.cacheDir, 50 * 1024 * 1024)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttp(
+        @AuthInterceptor authInterceptor: Interceptor,
+        @LoggingInterceptor loggingInterceptor: Interceptor,
+        cache: Cache
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .cache(cache)
+            .addInterceptor(authInterceptor)
+            .addInterceptor(loggingInterceptor)
+            .callTimeout(15, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    fun provideGsonConverterFactory(gson: Gson): GsonConverterFactory {
+        return GsonConverterFactory.create(gson)
+    }
+
+    @Provides
+    fun provideRetrofit(
+        client: dagger.Lazy<OkHttpClient>,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .callFactory {
+                client.get().newCall(it)
+            }
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGitHubService(retrofit: Retrofit): UnSplashService {
+        return retrofit.create(UnSplashService::class.java)
+    }
 }
---
+++ app/src/main/java/com/example/narcissusflower/di/Qualifiers.kt
@@ -0,0 +1,9 @@
+package com.example.narcissusflower.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+annotation class LoggingInterceptor
+
+@Qualifier
+annotation class AuthInterceptor
