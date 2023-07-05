5aca08672d873f2dcee627ff751a6cb753b1f3b8
HamidReza Farahani <h.farahani>
2023-07-05 11:27:34 +0430

    using navigation component, create home fragment

---
+++ app/src/main/java/com/example/narcissusflower/ui/home/HomeFragment.kt
@@ -0,0 +1,15 @@
+package com.example.narcissusflower.ui.home
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.narcissusflower.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/narcissusflower/ui/home/HomeViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.narcissusflower.ui.home
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle
+) : ViewModel() {
+}
