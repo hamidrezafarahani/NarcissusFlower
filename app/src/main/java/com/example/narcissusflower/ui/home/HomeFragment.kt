006b41fb39b2364780e961cf7f929f516ccb798c
HamidReza Farahani <h.farahani>
2023-07-05 11:58:38 +0430

    using binding delegate

--- app/src/main/java/com/example/narcissusflower/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/narcissusflower/ui/home/HomeFragment.kt
@@ -3,11 +3,17 @@ package com.example.narcissusflower.ui.home
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.narcissusflower.R
+import com.example.narcissusflower.databinding.FragmentHomeBinding
+import com.example.narcissusflower.utils.dataBindings
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private val binding by dataBindings(FragmentHomeBinding::bind)
+    private val viewModel by viewModels<HomeViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
---
+++ app/src/main/java/com/example/narcissusflower/utils/BindingDelegates.kt
@@ -0,0 +1,51 @@
+package com.example.narcissusflower.utils
+
+import android.view.View
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.viewbinding.ViewBinding
+
+inline fun <reified BindingT : ViewBinding> Fragment.viewBindings(
+    crossinline bind: (View) -> BindingT
+) = object : Lazy<BindingT> {
+
+    private var cached: BindingT? = null
+
+    private val observer = LifecycleEventObserver { _, event ->
+        if (event == Lifecycle.Event.ON_DESTROY) {
+            cached = null
+        }
+    }
+
+    override val value: BindingT
+        get() = cached ?: bind(requireView()).also {
+            viewLifecycleOwner.lifecycle.addObserver(observer)
+            cached = it
+        }
+
+    override fun isInitialized() = cached != null
+}
+
+inline fun <reified BindingT : ViewDataBinding> Fragment.dataBindings(
+    crossinline bind: (View) -> BindingT
+) = object : Lazy<BindingT> {
+
+    private var cached: BindingT? = null
+
+    private val observer = LifecycleEventObserver { _, event ->
+        if (event == Lifecycle.Event.ON_DESTROY) {
+            cached = null
+        }
+    }
+
+    override val value: BindingT
+        get() = cached ?: bind(requireView()).also {
+            it.lifecycleOwner = viewLifecycleOwner
+            viewLifecycleOwner.lifecycle.addObserver(observer)
+            cached = it
+        }
+
+    override fun isInitialized() = cached != null
+}
