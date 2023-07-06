0ab8a0ef88ab05c6cf37342323f458a2ca7078ae
HamidReza Farahani <h.farahani>
2023-07-06 13:23:54 +0430

    cmpl impl ui logic of garden fragment

--- app/src/main/java/com/example/narcissusflower/binding/BindingAdapters.kt
+++ app/src/main/java/com/example/narcissusflower/binding/BindingAdapters.kt
@@ -1,5 +1,6 @@
 package com.example.narcissusflower.binding
 
+import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
@@ -12,3 +13,7 @@ fun ImageView.bindFrom(url: String) = Glide.with(this)
     .into(this)
 
 
+@BindingAdapter("isGone")
+fun View.isGone(result: Boolean = false) {
+    visibility = if (result) View.GONE else View.VISIBLE
+}
--- app/src/main/java/com/example/narcissusflower/ui/garden/GardenFragment.kt
+++ app/src/main/java/com/example/narcissusflower/ui/garden/GardenFragment.kt
@@ -4,19 +4,71 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.viewpager2.widget.ViewPager2
 import com.example.narcissusflower.R
 import com.example.narcissusflower.databinding.FragmentGardenBinding
 import com.example.narcissusflower.binding.dataBindings
+import com.example.narcissusflower.data.local.entities.PlantAndGardenPlantings
+import com.example.narcissusflower.ui.home.HomeFragmentDirections
+import com.example.narcissusflower.ui.home.PLANT_LIST_PAGE_INDEX
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class GardenFragment : Fragment(R.layout.fragment_garden) {
 
     private val binding by dataBindings(FragmentGardenBinding::bind)
     private val viewModel by viewModels<GardenViewModel>()
+    private val navController: NavController = findNavController()
+    private lateinit var adapter: GardenPlantingAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        adapter = GardenPlantingAdapter(viewLifecycleOwner) {
+            navigateToPlantDetailFragment(it)
+        }
+
+        binding setupUi adapter
+        binding updateUi adapter
+    }
+
+    private infix fun FragmentGardenBinding.setupUi(adapter: GardenPlantingAdapter) {
+        hasPlantings = false
+        listGarden.adapter = adapter
+        addPlant.setOnClickListener {
+            navigateToPlantListFragment()
+        }
+    }
+
+    private infix fun FragmentGardenBinding.updateUi(adapter: GardenPlantingAdapter) {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.plantAndGardenPlantings.collectLatest {
+                    hasPlantings = it.isNotEmpty()
+                    adapter.submitList(it)
+                }
+            }
+        }
+    }
+
+
+    private fun navigateToPlantListFragment() {
+        // TODO: It's Not Good!
+        requireActivity().findViewById<ViewPager2>(R.id.view_pager)
+            .currentItem = PLANT_LIST_PAGE_INDEX
+    }
+
+    private fun navigateToPlantDetailFragment(plantings: PlantAndGardenPlantings) {
+        val plantId = plantings.plant.plantId
+        val direction = HomeFragmentDirections.actionHomeFragmentToPlantDetailFragment(plantId)
+        navController.navigate(direction)
     }
 
     companion object {
