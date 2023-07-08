91f42f6b5ebe657de9991bda1f405c0a816bce36
HamidReza Farahani <h.farahani>
2023-07-08 19:32:42 +0430

    in fragment detail xml using data binding impl plant detail view model start impl plant detail fragment

--- app/src/main/java/com/example/narcissusflower/ui/plantdetail/PlantDetailFragment.kt
+++ app/src/main/java/com/example/narcissusflower/ui/plantdetail/PlantDetailFragment.kt
@@ -3,14 +3,37 @@ package com.example.narcissusflower.ui.plantdetail
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.example.narcissusflower.R
+import com.example.narcissusflower.binding.dataBindings
+import com.example.narcissusflower.databinding.FragmentPlantDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PlantDetailFragment : Fragment(R.layout.fragment_plant_detail) {
 
+    private val binding by dataBindings(FragmentPlantDetailBinding::bind)
+    private val viewModel by viewModels<PlantDetailViewModel>()
+    private val navController: NavController = findNavController()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            plantDetailViewModel = viewModel
+            callback = Callback {
+                viewModel.addPlantToGarden()
+
+                // TODO: hide FAB
+                // TODO: show snack bar
+            }
+
+            galleryNav.setOnClickListener {
+                // TODO: Go to gallery fragment
+            }
+        }
     }
 
     companion object {
@@ -20,5 +43,10 @@ class PlantDetailFragment : Fragment(R.layout.fragment_plant_detail) {
                 arguments = Bundle().apply {}
             }
         }
+    }
+
+    fun interface Callback {
+
+        fun clickEvent()
     }
 }
--- app/src/main/java/com/example/narcissusflower/ui/plantdetail/PlantDetailViewModel.kt
+++ app/src/main/java/com/example/narcissusflower/ui/plantdetail/PlantDetailViewModel.kt
@@ -1,9 +1,39 @@
 package com.example.narcissusflower.ui.plantdetail
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.narcissusflower.data.repos.GardenPlantingRepository
+import com.example.narcissusflower.data.repos.PlantRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class PlantDetailViewModel @Inject constructor() : ViewModel() {
+class PlantDetailViewModel @Inject constructor(
+    private val plantRepository: PlantRepository,
+    private val gardenPlantingRepository: GardenPlantingRepository,
+    private val savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    val plantId = savedStateHandle.get<String>(PLANT_ID_SAVED_STATE_KEY)!!
+
+    val isPlanted = gardenPlantingRepository.isPlanted(plantId).asLiveData()
+
+    val plant = plantRepository.getPlant(plantId).shareIn(
+        scope = viewModelScope, started = SharingStarted.WhileSubscribed(), replay = 1
+    )
+
+    fun addPlantToGarden() {
+        viewModelScope.launch {
+            gardenPlantingRepository.createGardenPlating(plantId)
+        }
+    }
+
+    companion object {
+        private const val PLANT_ID_SAVED_STATE_KEY = "plantId"
+    }
 }
