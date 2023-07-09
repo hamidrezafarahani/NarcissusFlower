48797dc28bff3e20b52557b3a6435bba3bc63cb8
HamidReza Farahani <h.farahani>
2023-07-09 18:13:58 +0430

    continue impl fragment plant detail xml

--- app/src/main/java/com/example/narcissusflower/binding/BindingAdapters.kt
+++ app/src/main/java/com/example/narcissusflower/binding/BindingAdapters.kt
@@ -1,10 +1,14 @@
 package com.example.narcissusflower.binding
 
+import android.text.method.LinkMovementMethod
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.text.HtmlCompat
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 @BindingAdapter("imageFromUrl")
 fun ImageView.bindFrom(url: String) = Glide.with(this)
@@ -16,4 +20,20 @@ fun ImageView.bindFrom(url: String) = Glide.with(this)
 @BindingAdapter("isGone")
 fun View.isGone(result: Boolean = false) {
     visibility = if (result) View.GONE else View.VISIBLE
+}
+
+@BindingAdapter("isFabGone")
+fun FloatingActionButton.isFabGone(isGone: Boolean) = if (isGone) hide() else show()
+
+@BindingAdapter("renderHtml")
+fun TextView.bindFromHtml(description: String?) {
+    text = if (!description.isNullOrBlank()) {
+        movementMethod = LinkMovementMethod.getInstance()
+        HtmlCompat.fromHtml(description, HtmlCompat.FROM_HTML_MODE_COMPACT)
+    } else ""
+}
+
+@BindingAdapter("wateringText")
+fun TextView.bindWateringInterval(wateringInterval: Int) {
+    text = if (wateringInterval > 1) "every $wateringInterval days" else "every day"
 }
