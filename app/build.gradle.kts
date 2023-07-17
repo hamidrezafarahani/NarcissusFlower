0549e2f9e19b3b886ccb35cda02fdd1a108d683c
HamidReza Farahani <h.farahani>
2023-07-17 16:47:30 +0430

    using kotlin instead of id

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("androidx.navigation.safeargs.kotlin")
     id("com.google.dagger.hilt.android")
-    id("kotlin-kapt")
+    kotlin("kapt")
 }
 
 android {
