688e2b6774507e349b8050ba787793ae87e53e32
HamidReza Farahani <h.farahani>
2023-08-02 16:10:43 +0430

    impl home fragment

--- app/src/main/java/com/example/narcissusflower/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/narcissusflower/ui/home/HomeFragment.kt
@@ -2,11 +2,13 @@ package com.example.narcissusflower.ui.home
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.narcissusflower.R
-import com.example.narcissusflower.databinding.FragmentHomeBinding
 import com.example.narcissusflower.binding.dataBindings
+import com.example.narcissusflower.databinding.FragmentHomeBinding
+import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,5 +19,28 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            viewPager.adapter = PagerAdapter(this@HomeFragment)
+
+            TabLayoutMediator(tabs, viewPager) { tab, position ->
+                tab.setIcon(getTabIcon(position))
+                tab.text = getTabTitle(position)
+            }.attach()
+
+            (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        }
+    }
+
+    private fun getTabIcon(position: Int): Int = when (position) {
+        MY_GARDEN_PAGE_INDEX -> R.drawable.garden_tab_selector
+        PLANT_LIST_PAGE_INDEX -> R.drawable.plant_list_tab_selector
+        else -> throw IndexOutOfBoundsException()
+    }
+
+    private fun getTabTitle(position: Int): String? = when (position) {
+        MY_GARDEN_PAGE_INDEX -> "My Garden"
+        PLANT_LIST_PAGE_INDEX -> "Plant List"
+        else -> null
     }
 }
