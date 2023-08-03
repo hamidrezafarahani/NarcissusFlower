2b0e4ecd8f3592aa3e41771ee5e64f23b901a396
HamidReza Farahani <h.farahani>
2023-08-03 16:55:34 +0430

    using state flow instead live data

--- app/src/main/java/com/example/narcissusflower/ui/plantdetail/PlantDetailViewModel.kt
+++ app/src/main/java/com/example/narcissusflower/ui/plantdetail/PlantDetailViewModel.kt
@@ -2,11 +2,12 @@ package com.example.narcissusflower.ui.plantdetail
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.narcissusflower.data.repos.GardenPlantingRepository
 import com.example.narcissusflower.data.repos.PlantRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -19,9 +20,13 @@ class PlantDetailViewModel @Inject constructor(
 
     val plantId = savedStateHandle.get<String>(PLANT_ID_SAVED_STATE_KEY)!!
 
-    val isPlanted = gardenPlantingRepository.isPlanted(plantId).asLiveData()
+    val isPlanted = gardenPlantingRepository.isPlanted(plantId).stateIn(
+        scope = viewModelScope, started = SharingStarted.WhileSubscribed(), initialValue = false
+    )
 
-    val plant = plantRepository.getPlant(plantId).asLiveData()
+    val plant = plantRepository.getPlant(plantId).stateIn(
+        scope = viewModelScope, started = SharingStarted.WhileSubscribed(), initialValue = null
+    )
 
     fun addPlantToGarden() {
         viewModelScope.launch {
